ce2b10fdb29651bc0771b170051e3f0438720f68
jcolavita <joseacp57@gmail.com>
2024-10-31 10:44:57 -0400

    change navbar and footer from page to laout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import NavBar from "./components/navBar/navBar";
+import Footer from "./components/footer/footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,7 +26,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
